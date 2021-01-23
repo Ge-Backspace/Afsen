f360137926c4ec07c8b7f958e625d03a1717b18f
Ge-Backspace <ismailgo441@gmail.com>
2021-01-23 18:13:02 +0700

    Menambahkan Today Shift Seeder

---
+++ api/database/seeders/TodayShiftEmployee.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\ShiftEmployee;
+use Carbon\Carbon;
+use Illuminate\Database\Seeder;
+
+class TodayShiftEmployee extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        ShiftEmployee::create([
+            'company_id' => 1,
+            'employee_id' => 1,
+            'shift_id' => 1,
+            'date' => Carbon::today()
+        ]);
+    }
+}
