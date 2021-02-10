29fcfeed4510550bf831df8dc4a5b9e9b559969e
Ge-Backspace <ismailgo441@gmail.com>
2021-02-10 12:00:16 +0700

    Test File Upload

--- api/app/Http/Controllers/TestController.php
+++ api/app/Http/Controllers/TestController.php
@@ -2,15 +2,18 @@
 
 namespace App\Http\Controllers;
 
-
+use App\Helpers\Helper;
 use Illuminate\Http\Request;
 use App\Imports\ProductsImport;
 use App\Imports\ProductsExport;
 use App\Models\Checkin;
+use App\Models\Files\File;
 use App\Models\Office;
 use App\Models\Product;
 use Barryvdh\DomPDF\Facade as PDF;
 use Carbon\Carbon;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Validator;
 use Maatwebsite\Excel\Concerns\ToArray;
 use Maatwebsite\Excel\Facades\Excel;
 use PHPUnit\TextUI\XmlConfiguration\CodeCoverage\Report\Crap4j;
@@ -52,8 +55,22 @@ class TestController extends Controller
 
     public function test(Request $request)
     {
-        $office = Office::where('company_id', $request->company_id)->get(['lat', 'lng']);
-        $center = $this->getCenter($office);
-        return $this->resp(['data' => $office, 'center' => $center]);
+        $input = $request->only('company_id', 'shift_name', 'code', 'schedule_in', 'schedule_out');
+        $validator = Validator::make($input, [
+            'file' => 'file'
+        ], Helper::messageValidation());
+        if ($validator->fails()) {
+            return $this->resp(Helper::generateErrorMsg($validator->errors()->getMessages()), 'Failed Add Data', false, 401);
+        }
+        $file = $request->file('file');
+        $path = Storage::putFile('public/images', $file);
+        $files = [
+            'file_name' => $file->getClientOriginalName(),
+            'path' => $path,
+            'extension' => $file->getClientOriginalExtension(),
+            'size' => $file->getSize()
+        ];
+        $upload = File::create($files);
+        return $this->resp($upload);
     }
 }
